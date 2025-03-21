3cd608e2e21b7867e77f06ded8ed5a0e9556f147
Fabio <fabioroedel250@gmail.com>
2025-03-21 19:45:17 +0100

    random picker done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    //UI
+    //Toml lesen und schreiben können
+    //Datenstruktur mit Übungen die dann zufällig gewählt werden
+
+
+    let mut exercises: Vec<String> = Vec::new();
+    exercises.push(String::from("Pullup"));
+    exercises.push(String::from("Pushup"));
+    exercises.push(String::from("Situp"));
+    println!("{}", pick_random_exercise(exercises))
+}
+
+fn pick_random_exercise(exercises: Vec<String>) -> String {
+    let index: usize = rand::random_range(0..exercises.len());
+    exercises[index].clone()
+
 }
