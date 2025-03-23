2de231dfa0f317e52dde9c4dc2bfb94e9764327a
Fabio <fabioroedel250@gmail.com>
2025-03-23 16:32:38 +0100

    Added functions for adding reps and exercises

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use toml_edit::{DocumentMut, Item, Table, value};
-use std::io;
 use std::fs;
+
+mod funcs;
 
 fn main() {
 
-    //UI
     //Toml lesen und schreiben können
-    //Datenstruktur mit Übungen die dann zufällig gewählt werden
+    //übung hinzufügen
+    //übung entfernen
+    //übung aktivieren und deaktivieren
+    //profil ändern
+    //Toml lesen und zufällige übung wählen
+    //UI
 
-    let PATH_TO_CONFIG: &str = "config.toml";
+    const PATH_TO_CONFIG: &str = "config.toml";
+    const PATH_TO_DATA: &str = "exercise_data.toml";
     let toml_str: String = fs::read_to_string(PATH_TO_CONFIG).expect("Fehler beim lesen!");
     let mut doc: DocumentMut = toml_str.parse::<DocumentMut>().expect("Fehler beim parsen!");
 
@@ -48,57 +54,70 @@ fn main() {
     exercises.push(String::from("Pullup"));
     exercises.push(String::from("Pushup"));
     exercises.push(String::from("Situp"));
-    println!("{}", pick_random_exercise(exercises))
+    println!("{}", pick_random_exercise(exercises));
+    change_interval(PATH_TO_CONFIG);
 }
 
-fn change_interval(path_to_config: String) {
-    let toml_str: String = fs::read_to_string(path_to_config).expect("Fehler beim lesen!");
+fn add_exercise(path: &str, exercise: &str) {
+    let toml_str: String = fs::read_to_string(path).expect("Fehler beim lesen!");
     let mut doc: DocumentMut = toml_str.parse::<DocumentMut>().expect("Fehler beim parsen!");
 
-    let floor:String  = safe_string_input("From: ");
-    let ceil:String  = safe_string_input("To: ");
+    doc[exercise]["reps"] = value(0);
+    doc[exercise]["amount"] = value(0);
+    doc[exercise]["max"] = value(0);
+
+    fs::write(path, doc.to_string()).expect("Fehler beim Schreiben!");
+}
+
+fn add_reps(path: &str, exercise: &str, reps: i32) {
+    let toml_str: String = fs::read_to_string(path).expect("Fehler beim lesen!");
+    let mut doc: DocumentMut = toml_str.parse::<DocumentMut>().expect("Fehler beim parsen!");
+
+    if let Some(exercise_value) = doc.get(exercise) {
+        let reps: i64 = reps as i64;
+        let mut all_reps: i64 = 0;
+        let mut amount: i64 = 0;
+        let mut max: i64 = 0;
+
+        if let Some(reps_value) = exercise_value.get("reps") {
+            all_reps = reps_value.as_integer().unwrap();
+        }
+        if let Some(amount_value) = exercise_value.get("amount") {
+            amount = amount_value.as_integer().unwrap();
+        }
+        if let Some(max_value) = exercise_value.get("max") {
+            max = max_value.as_integer().unwrap();
+        }
+
+        if reps > 0 {
+            if reps > max {
+                max = reps;
+            }
+            amount += 1;
+            all_reps += reps;
+
+            doc[exercise]["max"] = value(max);
+            doc[exercise]["amount"] = value(amount);
+            doc[exercise]["reps"] = value(all_reps);
+
+            fs::write(path, doc.to_string()).expect("Fehler beim Schreiben!");
+        }
+    }
+
+
+
+}
+
+fn change_interval(path: &str) {
+    let toml_str: String = fs::read_to_string(path).expect("Fehler beim lesen!");
+    let mut doc: DocumentMut = toml_str.parse::<DocumentMut>().expect("Fehler beim parsen!");
+
+    let floor:String  = funcs::safe_string_input("From: ");
+    let ceil:String  = funcs::safe_string_input("To: ");
     doc["floor"] = value(floor);
     doc["ceil"] = value(ceil);
 
-}
-
-fn safe_number_input(question: &str) -> i32 {
-    loop {
-        let mut input: String = String::new();
-        println!("{}", question);
-        io::stdin().read_line(&mut input);
-        let input: &str = input.trim();
-
-        if let Ok(zahl) = input.parse::<i32>() {
-            return zahl;
-        }
-    }
-}
-
-fn safe_string_input(question: &str) -> String {
-    loop {
-        let mut input: String = String::new();
-        println!("{}", question);
-        io::stdin().read_line(&mut input);
-        let input: String = input.trim().to_string();
-
-        return input;
-    }
-}
-
-fn safe_option_input(question: &str, options: Option<&[&str]>) -> String {
-    loop {
-        let mut input: String = String::new();
-        println!("{}", question);
-        io::stdin().read_line(&mut input);
-        let input: &str = input.trim();
-
-        if let Some(options) = options {
-            if options.contains(&input) {
-                return input.to_string();
-            }
-        }
-    }
+    fs::write(path, doc.to_string()).expect("Fehler beim Schreiben!");
 }
 
 fn pick_random_exercise(exercises: Vec<String>) -> String {
