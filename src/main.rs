d2ef40e591a0bfc1620f706eb03339b329f7c48f
Fabio <fabioroedel250@gmail.com>
2025-03-23 18:47:14 +0100

    Added functinality to change exercise status and get all active exercises

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,115 @@
 use toml_edit::{DocumentMut, Item, Table, value};
 use std::fs;
-
 mod funcs;
+
+fn change_exercise_status(path: &str, exercise: &str) {
+    let toml_str: String = fs::read_to_string(path).expect("Fehler beim lesen!");
+    let mut doc: DocumentMut = toml_str.parse::<DocumentMut>().expect("Fehler beim parsen!");
+
+    let mut status: bool = true;
+
+    if let Some(exercises_table) = doc.get("exercises") {
+        if let Some(exercise_table) = exercises_table.get(exercise) {
+            if let Some(profiles_table) = exercise_table.get("profiles") {
+                if let Some(profile_value) = profiles_table.get("normal") {
+                    status = profile_value.as_bool().unwrap();
+                }
+            }
+        }
+    }
+
+    doc["exercises"][exercise]["profiles"]["normal"] = value(!status);
+
+    fs::write(path, doc.to_string()).expect("Fehler beim Schreiben!");
+}
+
+fn add_exercise(path: &str, exercise: &str) {
+    let toml_str: String = fs::read_to_string(path).expect("Fehler beim lesen!");
+    let mut doc: DocumentMut = toml_str.parse::<DocumentMut>().expect("Fehler beim parsen!");
+    
+    doc[exercise]["reps"] = value(0);
+    doc[exercise]["amount"] = value(0);
+    doc[exercise]["max"] = value(0);
+    
+    fs::write(path, doc.to_string()).expect("Fehler beim Schreiben!");
+}
+
+fn add_reps(path: &str, exercise: &str, reps: i32) {
+    let toml_str: String = fs::read_to_string(path).expect("Fehler beim lesen!");
+    let mut doc: DocumentMut = toml_str.parse::<DocumentMut>().expect("Fehler beim parsen!");
+
+    if let Some(exercise_value) = doc.get(exercise) {
+        let reps: i64 = reps as i64;
+        let mut all_reps: i64 = 0;
+        let mut amount: i64 = 0;
+        let mut max: i64 = 0;
+        
+        if let Some(reps_value) = exercise_value.get("reps") {
+            all_reps = reps_value.as_integer().unwrap();
+        }
+        if let Some(amount_value) = exercise_value.get("amount") {
+            amount = amount_value.as_integer().unwrap();
+        }
+        if let Some(max_value) = exercise_value.get("max") {
+            max = max_value.as_integer().unwrap();
+        }
+        
+        if reps > 0 {
+            if reps > max {
+                max = reps;
+            }
+            amount += 1;
+            all_reps += reps;
+            
+            doc[exercise]["max"] = value(max);
+            doc[exercise]["amount"] = value(amount);
+            doc[exercise]["reps"] = value(all_reps);
+            
+            fs::write(path, doc.to_string()).expect("Fehler beim Schreiben!");
+        }
+    }
+}
+
+fn change_interval(path: &str) {
+    let toml_str: String = fs::read_to_string(path).expect("Fehler beim lesen!");
+    let mut doc: DocumentMut = toml_str.parse::<DocumentMut>().expect("Fehler beim parsen!");
+
+    let floor:String  = funcs::safe_string_input("From: ");
+    let ceil:String  = funcs::safe_string_input("To: ");
+    doc["floor"] = value(floor);
+    doc["ceil"] = value(ceil);
+    
+    fs::write(path, doc.to_string()).expect("Fehler beim Schreiben!");
+}
+
+fn pick_random_exercise(exercises: Vec<String>) -> String {
+    let index: usize = rand::random_range(0..exercises.len());
+    exercises[index].clone()
+}
+
+fn get_exercises(path: &str) -> Vec<String> {
+    let toml_str: String = fs::read_to_string(path).expect("Fehler beim lesen!");
+    let mut doc: DocumentMut = toml_str.parse::<DocumentMut>().expect("Fehler beim parsen!");
+
+    let mut exercises: Vec<String> = Vec::new();
+
+    if let Some(exercises_table) = doc.get("exercises").and_then(|t| t.as_table()) {
+        for (exercise_name, exercise_data) in exercises_table.iter() {
+            if let Some(status) = exercise_data.get("profiles").unwrap().get("normal") {
+                if status.as_bool().unwrap() == true {
+                    exercises.push(String::from(exercise_name));
+                }
+            }
+        }
+    }
+
+    exercises
+}
 
 fn main() {
 
-    //Toml lesen und schreiben können
-    //übung hinzufügen
-    //übung entfernen
-    //übung aktivieren und deaktivieren
     //profil ändern
-    //Toml lesen und zufällige übung wählen
+    //profil hinzufügen/löschen
     //UI
 
     const PATH_TO_CONFIG: &str = "config.toml";
@@ -50,77 +149,5 @@ fn main() {
     println!("Erfolgreich geschrieben!");
 
 
-    let mut exercises: Vec<String> = Vec::new();
-    exercises.push(String::from("Pullup"));
-    exercises.push(String::from("Pushup"));
-    exercises.push(String::from("Situp"));
-    println!("{}", pick_random_exercise(exercises));
     change_interval(PATH_TO_CONFIG);
-}
-
-fn add_exercise(path: &str, exercise: &str) {
-    let toml_str: String = fs::read_to_string(path).expect("Fehler beim lesen!");
-    let mut doc: DocumentMut = toml_str.parse::<DocumentMut>().expect("Fehler beim parsen!");
-
-    doc[exercise]["reps"] = value(0);
-    doc[exercise]["amount"] = value(0);
-    doc[exercise]["max"] = value(0);
-
-    fs::write(path, doc.to_string()).expect("Fehler beim Schreiben!");
-}
-
-fn add_reps(path: &str, exercise: &str, reps: i32) {
-    let toml_str: String = fs::read_to_string(path).expect("Fehler beim lesen!");
-    let mut doc: DocumentMut = toml_str.parse::<DocumentMut>().expect("Fehler beim parsen!");
-
-    if let Some(exercise_value) = doc.get(exercise) {
-        let reps: i64 = reps as i64;
-        let mut all_reps: i64 = 0;
-        let mut amount: i64 = 0;
-        let mut max: i64 = 0;
-
-        if let Some(reps_value) = exercise_value.get("reps") {
-            all_reps = reps_value.as_integer().unwrap();
-        }
-        if let Some(amount_value) = exercise_value.get("amount") {
-            amount = amount_value.as_integer().unwrap();
-        }
-        if let Some(max_value) = exercise_value.get("max") {
-            max = max_value.as_integer().unwrap();
-        }
-
-        if reps > 0 {
-            if reps > max {
-                max = reps;
-            }
-            amount += 1;
-            all_reps += reps;
-
-            doc[exercise]["max"] = value(max);
-            doc[exercise]["amount"] = value(amount);
-            doc[exercise]["reps"] = value(all_reps);
-
-            fs::write(path, doc.to_string()).expect("Fehler beim Schreiben!");
-        }
-    }
-
-
-
-}
-
-fn change_interval(path: &str) {
-    let toml_str: String = fs::read_to_string(path).expect("Fehler beim lesen!");
-    let mut doc: DocumentMut = toml_str.parse::<DocumentMut>().expect("Fehler beim parsen!");
-
-    let floor:String  = funcs::safe_string_input("From: ");
-    let ceil:String  = funcs::safe_string_input("To: ");
-    doc["floor"] = value(floor);
-    doc["ceil"] = value(ceil);
-
-    fs::write(path, doc.to_string()).expect("Fehler beim Schreiben!");
-}
-
-fn pick_random_exercise(exercises: Vec<String>) -> String {
-    let index: usize = rand::random_range(0..exercises.len());
-    exercises[index].clone()
 }
