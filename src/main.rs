2dbd772ca268b46e3ad2ae20a0185b81c3d6a4d5
Knabe <fabioroedel250@gmail.com>
2025-06-19 00:10:42 +0200

    DataPoint Tooltips

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
+//#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
 #![allow(clippy::cast_possible_truncation)]
 #![allow(clippy::cast_precision_locc)]
 #![allow(clippy::too_many_lines)]
@@ -20,10 +20,10 @@ use once_cell::sync::Lazy;
 use std::sync::Mutex;
 
 //REMINDER src/...
-const PATH_TO_CONFIG: &str = "resources/config.toml";
-const PATH_TO_DATA: &str =   "resources/exercise_data.toml";
-const PATH_TO_CHRONO: &str = "resources/chronological_data.txt";
-const PATH_TO_SOUND: &str =  "resources/Sound.mp3";
+const PATH_TO_CONFIG: &str = "src/resources/config.toml";
+const PATH_TO_DATA: &str =   "src/resources/exercise_data.toml";
+const PATH_TO_CHRONO: &str = "src/resources/chronological_data.txt";
+const PATH_TO_SOUND: &str =  "src/resources/Sound.mp3";
 static mut VOLUME: f32 = 0.3;
 static VISUALISATION_DATA: Lazy<Mutex<(HashMap<String,(i32,i32,i32)>, HashMap<String, Vec<(NaiveDate, i32)>>)>> = Lazy::new(|| Mutex::new((HashMap::new(), HashMap::new())));
 
